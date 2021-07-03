badf529cb43cbe088de963c70225071931cf755d
Brigham Michaelis <brigham.michaelis@gmail.com>
2021-07-03 17:21:16 -0600

    Set up prelim input for game pad

---
+++ SSB_RETROGRADE_MAIN/SSB_RETROGRADE/Game1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Input.InputListeners;
+
+namespace SSB_RETROGRADE
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+
+        private readonly GamePadListener _gamePadListener;
+
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _gamePadListener = new GamePadListener();
+            Components.Add(new InputListenerComponent(this, _gamePadListener));
+
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            //_gamePadListener = new GamePadListener();
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _gamePadListener.ButtonDown += (sender, args) => { Window.Title = $"Key {args.Button} Down"; };
+
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SSB_RETROGRADE_MAIN/SSB_RETROGRADE/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SSB_RETROGRADE
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
