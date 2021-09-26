016d59ef3600386bf34d9ef01e73285092a70f2c
Brigham Michaelis <brigham.michaelis@gmail.com>
2021-09-25 22:27:22 -0600

    Make many thing just work

--- SSB_RETROGRADE_MAIN/SSB_RETROGRADE/Game1.cs
+++ SSB_RETROGRADE_MAIN/SSB_RETROGRADE/Game1.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended.Input.InputListeners;
-using System.Collections.Generic;
+using System;
 
 namespace SSB_RETROGRADE
 {
@@ -14,9 +14,13 @@ namespace SSB_RETROGRADE
 
         private readonly GamePadListener _gamePadListener1;
         private readonly GamePadListener _gamePadListener2;
+        private readonly KeyboardListener _keyboardListener;
 
         private GamePadListenerSettings gplsobj1;
         private GamePadListenerSettings gplsobj2;
+        private KeyboardListenerSettings kbls;
+
+        private Point camera;
 
 
 
@@ -49,7 +53,7 @@ namespace SSB_RETROGRADE
 
         private MassiveMarioManager marioInstance;
 
-
+        private Texture2D levelpng;
 
 
 
@@ -58,15 +62,22 @@ namespace SSB_RETROGRADE
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.GraphicsProfile = GraphicsProfile.HiDef;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
 
             gplsobj1 = new GamePadListenerSettings(PlayerIndex.One);
             gplsobj2 = new GamePadListenerSettings(PlayerIndex.Two);
+            kbls = new KeyboardListenerSettings();
             _gamePadListener1 = new GamePadListener(gplsobj1);
             _gamePadListener2 = new GamePadListener(gplsobj2);
+            _keyboardListener = new KeyboardListener(kbls);
             Components.Add(new InputListenerComponent(this, _gamePadListener1));
             Components.Add(new InputListenerComponent(this, _gamePadListener2));
+            Components.Add(new InputListenerComponent(this, _keyboardListener));
+
+
+            camera = new Point(0);
 
         }
 
@@ -78,16 +89,23 @@ namespace SSB_RETROGRADE
 
 
 
+
             base.Initialize();
+        }
+
+        private void _keyboardListener_KeyPressed(object sender, KeyboardEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            System.Console.WriteLine("About to create the mario manager");
+            //System.Console.WriteLine("About to create the mario manager");
 
             marioInstance = new MassiveMarioManager(Content.Load<Texture2D>("CharacterSprites/MarioAndLuigiSprites"));
+            levelpng = this.Content.Load<Texture2D>("Levels/smw1");
             marioInstance.isThisCorrect(_gamePadListener1);
 
 
@@ -106,13 +124,33 @@ namespace SSB_RETROGRADE
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                camera.X -= 10;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                camera.Y += 10;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                camera.X += 10;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                camera.Y -= 10;
+            }
+
             marioInstance.updateThisThing(_gamePadListener1);
 
 
             //_gamePadListener1.ButtonDown += (sender, args) => { Window.Title = $"Key {args.Button} Down"; };
             //_gamePadListener2.ButtonDown += (sender, args) => { Window.Title = $"P@P@P@P@P@@222Key {args.Button} Down"; };
 
-
+            
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -123,6 +161,47 @@ namespace SSB_RETROGRADE
             GraphicsDevice.Clear(Color.Black);
 
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, null, null, null, null);
+            /*
+             * The idea of a Camera.
+             * This camera is an abstraction of something else that is going on behind the scenes
+             * It must really be thought like unto the following.
+             * Imagine, a infinitly large canvas.
+             * upon this canvas can be placed any 2d image that we care about.
+             * Naturally there is a 0,0 point on this "Infinite" canvas, but for our purposes that doesn't matter
+             * Now that we have imagined this "infinite" canas that can receive any image we care about....
+             * Now consider this. 
+             * There is a window into this world of this canvas, and it has a widht and a height.
+             * This window can never move.
+             * Thus the illusion of movement is achieved when images are drawn, deleted, and redrawn at differnet points
+             * relative to the 0,0 point which just so happens to be the top left corner of the "game window"
+             * Now back to this idea of a camera.
+             * 
+             * Our camera should be able to do a few things.
+             * 1. Provide the illusion of any type of 2d translation
+             *      This is accomplished by redrawing our images at different locations on the canvas.
+             * 2. Zoom in and Zoom out.
+             *      This may be a little ambitious, but when zooming occurs,everything in the game window would be stretched to take up 
+             *      more room on this canvas or less room based on the desired effect.
+             *
+             *I guess we could talk about the camera, but that doesn't seem right.
+             *For example if you were playing a mario game, 
+             *and you where in the current level or in the menu, and you pushed left,... what would you expect?
+             *
+             *Would there be some grand abstracted manager thing moving the images, npc's, and buildings? (camera?)
+             *
+             *Or would there be a canas item manager, that could do that for you?
+             *How would you handle traps, pits, one-hit-kills, walls, floors, and ceilings?
+             *
+             *How would all these things play together and still be well coded?
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             */
+            _spriteBatch.Draw(levelpng,new Rectangle(0+camera.X,0+camera.Y,10240,1400),Color.White);
             marioInstance.drawMe(_spriteBatch);
             //_spriteBatch.Draw(marioSpriteTexture, new Rectangle(50,50,200,200), marioTangle[tangleCounter], Color.White);
             _spriteBatch.End();
@@ -130,6 +209,12 @@ namespace SSB_RETROGRADE
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+
+        public void handleKeyboardButtonDownInput(Object sender, GamePadEventArgs args)
+        {
+
         }
     }
 }
--- SSB_RETROGRADE_MAIN/SSB_RETROGRADE/MassiveMarioManager.cs
+++ SSB_RETROGRADE_MAIN/SSB_RETROGRADE/MassiveMarioManager.cs
@@ -9,6 +9,115 @@ using MonoGame.Extended.Input.InputListeners;
 
 namespace SSB_RETROGRADE
 {
+
+    enum AnimationStrategy
+    {
+        Cyclical, // 0,1,2,3,4,0,1,2,3,4...
+        ReverseCyclical, // 4,3,2,1,0,4,3,2,1,0...
+        PingPong // 0,1,2,3,4,3,2,1,2,3,4...
+    }
+
+    /**
+     * 
+     *  There are a number of things that will trigger a change to the currently displayed animation frame.
+     *  1. Controller input
+     *  2. Environment input enemies, spells, collisions with ceilings, and floors, spike traps, pits, lava, etc...
+     *  3. 
+     * 
+     */
+
+
+
+    class DrawingFramesManager
+    {
+        public List<Rectangle> listOfFrames;
+        public int currentFrameIndex;
+        public int FinalFrameIndex;
+        public AnimationStrategy currentAnimationStrategy;
+
+        public virtual Rectangle getNextFrame()
+        {
+            return new Rectangle();
+        }
+    }
+
+    class MarioStandingFramesManager : DrawingFramesManager
+    {
+
+        public MarioStandingFramesManager(List<Rectangle> inputList)
+        {
+            this.listOfFrames = inputList;
+            this.currentFrameIndex = 0;
+            this.currentAnimationStrategy = AnimationStrategy.Cyclical;
+        }
+        public override Rectangle getNextFrame()
+        {
+            if (this.currentFrameIndex + 1 >= this.listOfFrames.Count)
+            {
+                this.currentFrameIndex = 0;
+                return this.listOfFrames[this.currentFrameIndex];
+            }
+            else
+            {
+                Rectangle tangleToReturn = this.listOfFrames[this.currentFrameIndex];
+                this.currentFrameIndex++;
+                return tangleToReturn;
+            }
+            //return base.getNextFrame();
+        }
+    }
+
+    class MarioJumpingFramesManager : DrawingFramesManager
+    {
+
+        public MarioJumpingFramesManager(List<Rectangle> inputList)
+        {
+            this.listOfFrames = inputList;
+            this.currentFrameIndex = 0;
+            this.currentAnimationStrategy = AnimationStrategy.Cyclical;
+        }
+        public override Rectangle getNextFrame()
+        {
+            if (this.currentFrameIndex + 1 >= this.listOfFrames.Count)
+            {
+                this.currentFrameIndex = 0;
+                return this.listOfFrames[this.currentFrameIndex];
+            }
+            else
+            {
+                Rectangle tangleToReturn = this.listOfFrames[this.currentFrameIndex];
+                this.currentFrameIndex++;
+                return tangleToReturn;
+            }
+            //return base.getNextFrame();
+        }
+    }
+
+    class MarioWalkingFramesManager : DrawingFramesManager
+    {
+
+        public MarioWalkingFramesManager(List<Rectangle> inputList)
+        {
+            this.listOfFrames = inputList;
+            this.currentFrameIndex = 0;
+            this.currentAnimationStrategy = AnimationStrategy.Cyclical;
+        }
+        public override Rectangle getNextFrame()
+        {
+            if (this.currentFrameIndex + 1 >= this.listOfFrames.Count)
+            {
+                Rectangle anotherTangleToReturn = this.listOfFrames[this.currentFrameIndex];
+                this.currentFrameIndex = 0;
+                return anotherTangleToReturn;
+            } else
+            {
+                Rectangle tangleToReturn = this.listOfFrames[this.currentFrameIndex];
+                this.currentFrameIndex++;
+                return tangleToReturn;
+            }
+            //return base.getNextFrame();
+        }
+    }
  
 
     class MarioState
@@ -30,7 +139,7 @@ namespace SSB_RETROGRADE
         }
         public virtual void enter(MassiveMarioManager theMarioState)
         {
-
+            
         }
     }
 
@@ -42,14 +151,22 @@ namespace SSB_RETROGRADE
             //base.handleInput();
             if (daArgs.CurrentState.IsButtonDown(Buttons.A))
             {
+                //System.Console.WriteLine(daArgs.CurrentState.ToString());
+                //System.Console.WriteLine(daArgs.PreviousState.ToString());
+
                 return new JumpingState();
             }
-            return null;
+            if (daArgs.CurrentState.IsButtonDown(Buttons.DPadRight))
+            {
+                return new WalkingState();
+            }
+            return this;
         }
 
         public override void enter(MassiveMarioManager theMarioState)
         {
             theMarioState.setStandingGraphic();
+            theMarioState.forceUpdateThisThing();
         }
 
 
@@ -59,16 +176,35 @@ namespace SSB_RETROGRADE
     {
         public override MarioState handleInput(GamePadEventArgs daArgs)
         {
-            if (daArgs.CurrentState.IsButtonDown(Buttons.A))
+            if (daArgs.CurrentState.IsButtonUp(Buttons.A))
             {
                 return new StandingState();
             }
-            return null;
+            return this;
         }
 
         public override void enter(MassiveMarioManager theMarioState)
         {
             theMarioState.setJumpingGraphic();
+            theMarioState.forceUpdateThisThing();
+        }
+    }
+
+    class WalkingState : MarioState
+    {
+        public override MarioState handleInput(GamePadEventArgs daArgs)
+        {
+            if (daArgs.CurrentState.IsButtonUp(Buttons.DPadRight))
+            {
+                return new StandingState();
+            }
+            return this;
+        }
+
+        public override void enter(MassiveMarioManager theMarioState)
+        {
+            theMarioState.setWalkingGraphic();
+            theMarioState.forceUpdateThisThing();
         }
     }
 
@@ -87,9 +223,20 @@ namespace SSB_RETROGRADE
         private Rectangle marioJump;
 
         private MarioState state_;
+        private DrawingFramesManager dfm_;
 
+        private Rectangle currentMarioFrame;
+        private List<Rectangle> currentMarioFrames;
 
-        private Rectangle currentMarioDrawingBox;
+        private List<Rectangle> marioStandingFrames;
+        private List<Rectangle> marioJumpingFrames;
+        private List<Rectangle> marioWalkingFrames;
+        private MarioStandingFramesManager marioStandingFramesManager;
+        private MarioJumpingFramesManager marioJumpingFramesManager;
+        private MarioWalkingFramesManager marioWalkingFramesManager;
+
+        private int rollingAnimationInt;
+        private Rectangle rectToDraw;
 
 
 
@@ -108,47 +255,59 @@ namespace SSB_RETROGRADE
             marioChangeDirection = new Rectangle(new Point(98, 9), new Point(16, 16));
             marioJump = new Rectangle(new Point(119, 9), new Point(16, 16));
 
+            marioStandingFrames = new List<Rectangle>();
+            marioJumpingFrames = new List<Rectangle>();
+            marioWalkingFrames = new List<Rectangle>();
 
+            marioStandingFrames.Add(marioStanding);
+            marioJumpingFrames.Add(marioJump);
+            marioWalkingFrames.Add(marioWalking1);
+            marioWalkingFrames.Add(marioWalking2);
+            marioWalkingFrames.Add(marioWalking3);
+
+            marioStandingFramesManager = new MarioStandingFramesManager(marioStandingFrames);
+            marioJumpingFramesManager = new MarioJumpingFramesManager(marioJumpingFrames);
+            marioWalkingFramesManager = new MarioWalkingFramesManager(marioWalkingFrames);
+
+            rollingAnimationInt = 0;
+
+
+
+
+
+
+            dfm_ = new DrawingFramesManager();
             state_ = new StandingState();
-            currentMarioDrawingBox = marioStanding;
+            currentMarioFrames = marioStandingFrames;
+            currentMarioFrame = currentMarioFrames[0];
+            rectToDraw = new Rectangle();
+            rectToDraw = dfm_.getNextFrame();
 
         }
 
-        
 
-        //private void handleInput(GamePadEventArgs daArgs)
-        //{
-        //    MarioState newState = state_.handleInput(daArgs);
-        //    if (newState != null)
-        //    {
-        //        state_ = newState;
-        //        state_.enter(this);
-        //    }
-        //}
         public void isThisCorrect(GamePadListener someGamePad)
         {
             someGamePad.ButtonDown += handleButtonDownInput;
+            someGamePad.ButtonUp += handleButtonUpInput;
+
         }
 
 
         public void updateThisThing(GamePadListener someGamePad)
         {
-            //MarioState newState = state_.handleInput(someGamePad);
-            //if (newState != null)
-            //{
-            //    state_ = newState;
-            //    state_.enter(this);
-            //}
+            rollingAnimationInt++;
+            if (rollingAnimationInt % 5 == 0)
+            {
+                forceUpdateThisThing();
+            }
 
-            
-
-            //someGamePad.ButtonDown += (sender, args) =>
-            //{
-            //    handleInput(args);
-            //    System.Console.WriteLine(args.Button);
-            //    System.Console.WriteLine("Not sure what is happening!");
-            //};
         }
+
+        public void forceUpdateThisThing()
+        {
+            rectToDraw = dfm_.getNextFrame();
+        }           
 
         public void handleButtonDownInput(Object sender, GamePadEventArgs args)
         {
@@ -161,27 +320,43 @@ namespace SSB_RETROGRADE
             }
         }
 
+        public void handleButtonUpInput(Object sender, GamePadEventArgs args)
+        {
+            MarioState newState = state_.handleInput(args);
+            if (newState != null)
+            {
+                state_ = newState;
+                state_.enter(this);
+            }
+        }
+
 
         public void drawMe(SpriteBatch spriteBatch)
         {
-            Rectangle tempRectangle = getCurretntMarioDrawingBox();
-            spriteBatch.Draw(marioSpriteAtlasPNG, new Rectangle(50, 50, 200, 200), tempRectangle, Color.White);
+            spriteBatch.Draw(marioSpriteAtlasPNG, new Rectangle(150, 50, 100, 200), rectToDraw, Color.White);
         }
 
         public void setStandingGraphic()
         {
-            currentMarioDrawingBox = marioStanding;
-            Console.WriteLine("mario Stood!!");
+            //currentMarioFrames = marioStandingFrames;
+            dfm_ = marioStandingFramesManager;
+            //Console.WriteLine("mario Stood!!");
         }
         public void setJumpingGraphic()
         {
-            currentMarioDrawingBox = marioJump;
-            System.Console.WriteLine("mario Jumped!");
+            //currentMarioFrames = marioJumpingFrames;
+            dfm_ = marioJumpingFramesManager;
+            //System.Console.WriteLine("mario Jumped!");
+        }
+
+        public void setWalkingGraphic()
+        {
+            dfm_ = marioWalkingFramesManager;
         }
 
         private Rectangle getCurretntMarioDrawingBox()
         {
-            return currentMarioDrawingBox;
+            return dfm_.getNextFrame();
         }
 
     }
